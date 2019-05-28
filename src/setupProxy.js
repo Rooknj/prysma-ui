83ba52a0b44f72a2a93818ff8a58ed1060ed0d7c
Nick Rook <Nick_Rook@intuit.com>
2019-05-27 17:42:25 -0700

    moved proxy to the right location

---
+++ src/setupProxy.js
@@ -0,0 +1,6 @@
+const proxy = require("http-proxy-middleware");
+const target = process.env.PROXY || "http://prysma.local"; //"http://localhost:4001"
+
+module.exports = function(app) {
+  app.use(proxy("/graphql", { target, ws: true }));
+};
