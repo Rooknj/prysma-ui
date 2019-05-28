b34d136d9973de264bb8e653ebd1f1e5bd4d0189
Nick Rook <Nick_Rook@intuit.com>
2019-05-27 21:27:22 -0700

    use CssBaseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { ApolloProvider } from "react-apollo-hooks"; // Connect Apollo to React
 import client from "./apolloClient";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import Main from "./main";
 
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <Main />
+      <CssBaseline>
+        <Main />
+      </CssBaseline>
     </ApolloProvider>
   );
 };
