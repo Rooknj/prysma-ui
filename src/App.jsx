7b03c982dedeb62e448a750ddd012f2bcd92be08
Nick Rook <Nick_Rook@intuit.com>
2019-05-27 21:33:15 -0700

    added styled components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { ApolloProvider } from "react-apollo-hooks"; // Connect Apollo to React
 import client from "./apolloClient";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { StylesProvider } from "@material-ui/styles";
 import Main from "./main";
 
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <CssBaseline>
-        <Main />
-      </CssBaseline>
+      <StylesProvider injectFirst>
+        <CssBaseline>
+          <Main />
+        </CssBaseline>
+      </StylesProvider>
     </ApolloProvider>
   );
 };
