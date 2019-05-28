85f3bfa0ae42d6da1092ce06ea1c2d8da270d033
Nick Rook <Nick_Rook@intuit.com>
2019-05-27 20:07:48 -0700

    added refetch button

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -3,19 +3,27 @@ import { useQuery } from "react-apollo-hooks";
 import { LIGHTS } from "common/graphqlConstants.js";
 
 const Main = () => {
-  const { data, error, loading, networkStatus } = useQuery(LIGHTS, {
+  const { data, error, loading, networkStatus, refetch } = useQuery(LIGHTS, {
     fetchPolicy: "cache-and-network",
     notifyOnNetworkStatusChange: true
   });
 
-  if (loading || networkStatus === 4) return <div>Loading...</div>;
-  if (error) return <div>Error</div>;
-  return data.lights.map(light => (
-    <div key={light.id}>
-      <span>{JSON.stringify(light)}</span>
-      <hr />
+  let Body;
+  if (loading || networkStatus === 4) Body = <div>Loading...</div>;
+  else if (error) Body = <div>Error</div>;
+  else
+    Body = data.lights.map(light => (
+      <div key={light.id}>
+        <span>{JSON.stringify(light)}</span>
+        <hr />
+      </div>
+    ));
+  return (
+    <div>
+      <button onClick={() => refetch()}>Refetch</button>
+      {Body}
     </div>
-  ));
+  );
 };
 
 export default Main;
