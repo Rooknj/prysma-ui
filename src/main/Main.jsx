bdb59f3d4294aef3637750ef7e23eac57063f372
Nick Rook <Nick_Rook@intuit.com>
2019-05-27 19:59:16 -0700

    updated to use react-apollo-hooks

--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,26 +1,21 @@
 import React from "react";
-import { Query } from "react-apollo";
+import { useQuery } from "react-apollo-hooks";
 import { LIGHTS } from "common/graphqlConstants.js";
 
 const Main = () => {
-  return (
-    <Query
-      query={LIGHTS}
-      fetchPolicy="cache-and-network"
-      notifyOnNetworkStatusChange
-    >
-      {({ loading, error, data, subscribeToMore, refetch, networkStatus }) => {
-        if (loading || networkStatus === 4) return <div>Loading...</div>;
-        if (error) return <div>Error</div>;
-        return data.lights.map(light => (
-          <div key={light.id}>
-            <span>{JSON.stringify(light)}</span>
-            <hr />
-          </div>
-        ));
-      }}
-    </Query>
-  );
+  const { data, error, loading, networkStatus } = useQuery(LIGHTS, {
+    fetchPolicy: "cache-and-network",
+    notifyOnNetworkStatusChange: true
+  });
+
+  if (loading || networkStatus === 4) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
+  return data.lights.map(light => (
+    <div key={light.id}>
+      <span>{JSON.stringify(light)}</span>
+      <hr />
+    </div>
+  ));
 };
 
 export default Main;
