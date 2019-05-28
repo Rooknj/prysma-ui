995ddeea2abc67c834c9966bd49882a8754ca31e
Nick Rook <Nick_Rook@intuit.com>
2019-05-27 21:36:41 -0700

    provided a theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import { ApolloProvider } from "react-apollo-hooks"; // Connect Apollo to React
 import client from "./apolloClient";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { StylesProvider } from "@material-ui/styles";
+import { ThemeProvider } from "styled-components";
+import { createMuiTheme } from "@material-ui/core/styles";
 import Main from "./main";
+
+const theme = createMuiTheme();
 
 const App = () => {
   return (
     <ApolloProvider client={client}>
       <StylesProvider injectFirst>
-        <CssBaseline>
-          <Main />
-        </CssBaseline>
+        <ThemeProvider theme={theme}>
+          <CssBaseline>
+            <Main />
+          </CssBaseline>
+        </ThemeProvider>
       </StylesProvider>
     </ApolloProvider>
   );
