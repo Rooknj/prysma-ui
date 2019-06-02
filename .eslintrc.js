8a1401692e555df4998edb18dc6c1ca3c3c707bb
Nick Rook <Nick_Rook@intuit.com>
2019-06-02 15:14:21 -0700

    added a real eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
-  extends: ["react-app"],
-  rules: {}
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["eslint:recommended", "react-app"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  rules: {
+    "no-console": "warn",
+    "no-unused-vars": "warn"
+  }
 };
