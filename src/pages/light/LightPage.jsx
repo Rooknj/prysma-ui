e402a3840a3fb0bf02767410b9f7b936e97cb856
Nick Rook <Nick_Rook@intuit.com>
2019-06-02 13:50:50 -0700

    finished LightPage

--- src/pages/light/LightPage.jsx
+++ src/pages/light/LightPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import {
   useLight,
   useSetLightState,
@@ -19,16 +19,19 @@ const StyledDiv = styled.div`
   align-items: center;
 `;
 
-const AboutPage = props => {
+const LightPage = props => {
   const { match } = props;
 
   const [newName, setNewName] = React.useState("");
+  const [removed, setRemoved] = React.useState(false);
 
   const lightId = match.params.id;
   const { data, error, loading } = useLight(lightId);
   const setLight = useSetLight();
   const setLightState = useSetLightState();
   const removeLight = useRemoveLight();
+
+  console.log(data, error, loading);
 
   const handleNameChange = e => {
     setNewName(e.target.value);
@@ -41,7 +44,7 @@ const AboutPage = props => {
   };
 
   const handleRemoveLight = () => {
-    removeLight(lightId);
+    removeLight(lightId).then(() => setRemoved(true));
   };
 
   const handleOnChange = currentLightState => e => {
@@ -69,6 +72,22 @@ const AboutPage = props => {
     setLightState(currentLightState.id, newLightState, currentLightState);
   };
 
+  const handleEffectChange = (effect, currentLightState) => () => {
+    const newLightState = {
+      effect
+    };
+
+    setLightState(currentLightState.id, newLightState, currentLightState);
+  };
+
+  const handleSpeedChange = currentLightState => speed => {
+    const newLightState = {
+      speed
+    };
+
+    setLightState(currentLightState.id, newLightState, currentLightState);
+  };
+
   let Body;
   if (loading) {
     Body = <Typography variant="body1">Loading...</Typography>;
@@ -89,10 +108,20 @@ const AboutPage = props => {
       colorOrder,
       stripType
     } = light;
-    const { connected, on, brightness, color } = light.state;
+    const { connected, on, brightness, color, effect, speed } = light.state;
     Body = (
       <div>
         <Typography variant="h3">{light.name}</Typography>
+        <Typography variant="h6">{"Rename the light: "}</Typography>
+        <TextField
+          placeholder="New Light Name"
+          value={newName}
+          color="primary"
+          onChange={handleNameChange}
+        />
+        <Button onClick={handleRenameLight} color="primary">
+          Rename
+        </Button>
         <Typography variant="h6">{`Connected: ${connected}`}</Typography>
         <StyledDiv>
           <Typography variant="h6">{"On: "}</Typography>
@@ -111,6 +140,7 @@ const AboutPage = props => {
           step={1}
           onChange={handleBrightnessChange(light.state)}
           disabled={!connected}
+          color="primary"
         />
         <Typography variant="h6">{"Color: "}</Typography>
         <CircleColorPicker
@@ -119,19 +149,31 @@ const AboutPage = props => {
           height={320}
           width={320}
         />
-        <Typography variant="h6">{"Rename the light: "}</Typography>
-        <TextField
-          placeholder="New Light Name"
-          value={newName}
-          onChange={handleNameChange}
+        <Typography variant="h6">{`Current Effect: ${effect}`}</Typography>
+        {supportedEffects &&
+          supportedEffects.map(supportedEffect => (
+            <Button
+              key={supportedEffect}
+              disabled={!connected}
+              color="primary"
+              onClick={handleEffectChange(supportedEffect, light)}
+            >
+              {supportedEffect}
+            </Button>
+          ))}
+        <Typography variant="h6">{"Effect Speed:"}</Typography>
+        <Slider
+          value={speed}
+          min={1}
+          max={7}
+          step={1}
+          onChange={handleSpeedChange(light.state)}
+          color="primary"
+          disabled={!connected}
         />
-        <Button onClick={handleRenameLight}>Rename</Button>
-        <Typography variant="h6">{"Remove the light: "}</Typography>
-        <Button onClick={handleRemoveLight}>Remove</Button>
         <Typography variant="h6">{"Config Info:"}</Typography>
         <Typography variant="body1">{`ID: ${id}`}</Typography>
         <Typography variant="body1">{`Name: ${name}`}</Typography>
-        <Typography variant="body1">{`Supported Effects: ${supportedEffects}`}</Typography>
         <Typography variant="body1">{`IP Address: ${ipAddress}`}</Typography>
         <Typography variant="body1">{`MAC Address: ${macAddress}`}</Typography>
         <Typography variant="body1">{`Number of LEDs: ${numLeds}`}</Typography>
@@ -140,6 +182,11 @@ const AboutPage = props => {
         <Typography variant="body1">{`Hardware: ${hardware}`}</Typography>
         <Typography variant="body1">{`Color Order: ${colorOrder}`}</Typography>
         <Typography variant="body1">{`Strip Type: ${stripType}`}</Typography>
+        <Typography variant="h6">{"Remove the light: "}</Typography>
+        <Button onClick={handleRemoveLight} color="primary">
+          Remove
+        </Button>
+        {removed && <Redirect push to="/" />}
       </div>
     );
   }
@@ -147,11 +194,11 @@ const AboutPage = props => {
   return (
     <div>
       <Link to={"/"}>
-        <Typography variant="body1">{"< Back"}</Typography>
+        <Button>{"< Back"}</Button>
       </Link>
       {Body}
     </div>
   );
 };
 
-export default AboutPage;
+export default LightPage;
