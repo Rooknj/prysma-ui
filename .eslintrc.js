ad1eda9c04d7eeaed170cba655f87e7c1bec891b
Nick Rook <Nick_Rook@intuit.com>
2019-06-02 19:25:31 -0700

    added eslint comments plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     // Shared Configs
     "eslint:recommended",
+    "plugin:eslint-comments/recommended",
     // Project Specific Configs
     "react-app",
     "airbnb",
