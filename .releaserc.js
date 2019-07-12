a09a6fb5d28e5a12585d8e28bfad5f55c4cfc8b4
Nick Rook <Nick_Rook@intuit.com>
2019-07-12 14:28:06 -0700

    chore: added commitlint

---
+++ .releaserc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm", // Does not publish by default if private is set to true
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
