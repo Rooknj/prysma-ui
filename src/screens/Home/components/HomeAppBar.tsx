ebd86f390d985cf1c454ed24dd2a2facc25ba49e
Nick Rook <Nick_Rook@intuit.com>
2019-08-11 19:03:38 -0700

    feat: added navigation to addLight button

--- src/screens/Home/components/HomeAppBar.tsx
+++ src/screens/Home/components/HomeAppBar.tsx
@@ -6,6 +6,7 @@ import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import AddIcon from "@material-ui/icons/Add";
 import prysmaLogo from "assets/images/prysma-logo.svg";
+import { LinkProps, Link } from "react-router-dom";
 
 const StyledImg = styled.img`
   height: 32px;
@@ -16,6 +17,15 @@ const StyledToolbar = styled(Toolbar)`
   align-items: center;
 `;
 
+/* TODO: Remove the forwardRef when available
+  The usage of React.forwardRef will no longer be required for react-router-dom v6.
+  see https://github.com/ReactTraining/react-router/issues/6056
+*/
+const CollisionLink = React.forwardRef<HTMLAnchorElement, Omit<LinkProps, "innerRef" | "to">>(
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type, @typescript-eslint/no-explicit-any
+  (props, ref) => <Link innerRef={ref as any} to="/addLight" {...props} />
+);
+
 const HomeAppBar = (): React.FunctionComponentElement<{}> => {
   return (
     <AppBar position="static" color="default">
@@ -24,7 +34,7 @@ const HomeAppBar = (): React.FunctionComponentElement<{}> => {
           <MenuIcon />
         </IconButton>
         <StyledImg src={prysmaLogo} alt="Prysma Logo" />
-        <IconButton edge="end" color="inherit" aria-label="menu">
+        <IconButton edge="end" color="inherit" aria-label="add light" component={CollisionLink}>
           <AddIcon />
         </IconButton>
       </StyledToolbar>
