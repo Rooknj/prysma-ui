05b1e25f0a04e82b3a50feae80d9185dd9eaf138
Nick Rook <rooknj@outlook.com>
2019-08-28 21:31:20 -0400

    feat: implemented home screen

--- src/screens/Home/components/HomeLight.tsx
+++ src/screens/Home/components/HomeLight.tsx
@@ -1,17 +1,70 @@
-import React, { ChangeEvent } from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
-import Switch from "@material-ui/core/Switch";
-import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import styled from "styled-components";
-import { useRemoveLightMutation, Light, useChangeOnMutation } from "generated/graphql";
-import { removeLightFromCache } from "lib/graphqlHelpers";
+import { Light, useChangeOnMutation } from "generated/graphql";
 import routes from "lib/routes";
-import { Typography } from "@material-ui/core";
+import { Fab } from "@material-ui/core";
+import PowerIcon from "@material-ui/icons/PowerSettingsNew";
+import DisconnectedIcon from "@material-ui/icons/PowerOff";
+import NavIcon from "@material-ui/icons/ChevronRight";
 import { useThrottledMutation } from "lib/hooks";
 
 const StyledDiv = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
+`;
+
+const PowerButton = styled(({ on, ...props }) => <Fab {...props} />)<{ on: boolean }>`
+  height: 64px;
+  width: 64px;
+  margin-bottom: 8px;
+  background-color: ${({ on, theme }) => (on ? theme.palette.primary.main : "#3B3B3B")};
+  color: ${({ on, theme }) =>
+    theme.palette.getContrastText(on ? theme.palette.primary.main : "#3B3B3B")};
+
+  :hover {
+    background-color: ${({ on, theme }) => (on ? theme.palette.primary.main : "#3B3B3B")};
+  }
+`;
+
+const StyledPowerIcon = styled(PowerIcon)`
+  height: 28px;
+  width: 28px;
+`;
+
+const NavButton = styled(Button)`
+  width: 104px;
+  padding-left: 0px;
+  padding-right: 0px;
+  padding-top: 2px;
+  padding-bottom: 2px;
+  height: 36px;
+  .MuiButton-label {
+    display: inline-grid;
+    grid-template-areas: ". text arrow";
+    grid-template-columns: 12px 1fr 12px;
+  }
+  background-color: #2c2c2c;
+  color: ${({ theme }) => theme.palette.common.white};
+`;
+
+const NavText = styled.div`
+  grid-area: text;
+  font-style: normal;
+  font-weight: normal;
+  text-transform: none;
+  max-height: 32px;
+  line-height: 16px;
+  font-size: 12px;
+  overflow: hidden;
+  word-break: break-all;
+`;
+
+const StyledNavIcon = styled(NavIcon)`
+  grid-area: arrow;
+  margin-left: -8px;
 `;
 
 export interface HomeLightProps extends Light {}
@@ -19,46 +72,46 @@ export interface HomeLightProps extends Light {}
 const HomeLight = (props: HomeLightProps): React.FunctionComponentElement<HomeLightProps> => {
   const { id, on, connected, name } = props;
 
-  const [removeLight] = useRemoveLightMutation();
+  const [toLight, setToLight] = useState(false);
   const [changeOn] = useChangeOnMutation();
 
   const throttledChangeOn = useThrottledMutation(changeOn);
 
-  const handleRemoveLight = (): void => {
-    removeLight({
-      variables: { id },
-      update: (proxy, { data: removeLightData }): void => {
-        if (!removeLightData || !removeLightData.removeLight) return;
+  const handleNavigateToLight = (): void => setToLight(true);
 
-        const lightToRemove = removeLightData.removeLight;
-        removeLightFromCache(proxy, lightToRemove);
-      },
-    });
-  };
-
-  const handleStateChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    const newOn = e.target.checked;
-
+  const handleToggleLight = (): void => {
     throttledChangeOn({
-      variables: { id, lightData: { on: newOn } },
+      variables: { id, lightData: { on: !on } },
       optimisticResponse: {
         __typename: "Mutation",
         setLight: {
           __typename: "Light",
           id,
-          on: newOn,
+          on: !on,
         },
       },
     });
   };
 
+  if (toLight) {
+    return <Redirect push to={`${routes.light}/${id}`} />;
+  }
+
   return (
     <StyledDiv>
-      <Button onClick={handleRemoveLight}>Remove</Button>
-      <Switch checked={on} onChange={handleStateChange} disabled={!connected} color="primary" />
-      <Link to={`${routes.light}/${id}`}>
-        <Typography variant="body1">{name}</Typography>
-      </Link>
+      <PowerButton
+        on={on}
+        aria-label={name}
+        disabled={!connected}
+        onClick={handleToggleLight}
+        disableRipple
+      >
+        {connected ? <StyledPowerIcon /> : <DisconnectedIcon />}
+      </PowerButton>
+      <NavButton variant="contained" onClick={handleNavigateToLight}>
+        <NavText>{name}</NavText>
+        <StyledNavIcon />
+      </NavButton>
     </StyledDiv>
   );
 };
--- src/screens/Home/components/LightList.tsx
+++ src/screens/Home/components/LightList.tsx
@@ -9,6 +9,14 @@ const StyledTypography = styled(Typography)`
   margin-bottom: 16px;
 `;
 
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-row-gap: 24px;
+  margin-left: 16px;
+  margin-right: 16px;
+`;
+
 export interface LightListProps {
   lights: Light[];
 }
@@ -21,11 +29,13 @@ const LightList = (props: LightListProps): React.FunctionComponentElement<LightL
       <StyledTypography variant="h5" align="center">
         Lights
       </StyledTypography>
-      {lights.map(
-        (light): React.FunctionComponentElement<{}> => (
-          <HomeLight key={light.id} {...light} />
-        )
-      )}
+      <GridContainer>
+        {lights.map(
+          (light): React.FunctionComponentElement<{}> => (
+            <HomeLight key={light.id} {...light} />
+          )
+        )}
+      </GridContainer>
     </Fragment>
   );
 };
