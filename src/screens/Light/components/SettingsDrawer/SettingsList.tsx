660c04f796e40ecab74b3dedd34f6c6a53790db9
Nick Rook <rooknj@outlook.com>
2019-09-01 15:40:03 -0400

    feat: added basic sections

--- src/screens/Light/components/SettingsDrawer/SettingsList.tsx
+++ src/screens/Light/components/SettingsDrawer/SettingsList.tsx
@@ -1,23 +1,76 @@
 import React from "react";
-import { List, ListItem, ListItemText, Divider } from "@material-ui/core";
+import { List, ListItem, ListItemText, Divider, ListSubheader } from "@material-ui/core";
+import styled from "styled-components";
+
+const StyledUl = styled.ul`
+  padding: 0;
+  background-color: "inherit";
+`;
+
+const StyledListSubheader = styled(ListSubheader)`
+  background-color: ${({ theme }) => theme.palette.background.default};
+`;
 
 const SettingsList = (): React.FunctionComponentElement<{}> => {
   return (
-    <List component="nav">
-      <ListItem button>
-        <ListItemText primary="Inbox" />
-      </ListItem>
-      <Divider />
-      <ListItem button divider>
-        <ListItemText primary="Drafts" />
-      </ListItem>
-      <ListItem button>
-        <ListItemText primary="Trash" />
-      </ListItem>
-      <Divider light />
-      <ListItem button>
-        <ListItemText primary="Spam" />
-      </ListItem>
+    <List subheader={<li />}>
+      <li>
+        <StyledUl>
+          <StyledListSubheader>Display</StyledListSubheader>
+          <Divider />
+          <ListItem>
+            <ListItemText primary="Name" />
+          </ListItem>
+          <Divider />
+        </StyledUl>
+      </li>
+      <li>
+        <StyledUl>
+          <StyledListSubheader>Config</StyledListSubheader>
+          <Divider />
+          <ListItem>
+            <ListItemText primary="ID" />
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemText primary="Firmware Version" />
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemText primary="Hardware" />
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemText primary="Color Order" />
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemText primary="IP Address" />
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemText primary="MAC Address" />
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemText primary="Number of LEDs" />
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemText primary="UDP Port" />
+          </ListItem>
+          <Divider />
+        </StyledUl>
+      </li>
+      <li>
+        <StyledUl>
+          <StyledListSubheader>Danger Zone</StyledListSubheader>
+          <Divider />
+          <ListItem button>
+            <ListItemText primary="Remove Light" />
+          </ListItem>
+        </StyledUl>
+      </li>
     </List>
   );
 };
